b208d4410af6add87ae0f40c7ca77ddf6eafc9f3
TalzoorBGU <zoor@post.bgu.ac.il>
2018-08-15 00:39:35 +0300

    "added Service install"

--- RPI_Service/serv_inst.sh
+++ RPI_Service/serv_inst.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Script to create 'x' service
 
-SERVICE_NAME = TEST.service
-DESCRIPTION = TESTING
-PYTHON_FILE = /home/pi/PythonScripts/Hydro_Pi/TEMP/Pi_switch/Pi_switch_main.py
+SERVICE_NAME="TEST.service"
+DESCRIPTION =TESTING"
+PYTHON_FILE"="/home/pi/PythonScripts/Hydro_Pi/TEMP/Pi_switch/Pi_switch_main.py"
 
 cd /lib/systemd/system/
 sudo sh -c 'echo "
