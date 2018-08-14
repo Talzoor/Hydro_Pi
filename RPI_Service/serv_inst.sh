7f915eee4dc9778b9bbb0678c891abb2d5c99831
TalzoorBGU <zoor@post.bgu.ac.il>
2018-08-15 01:22:00 +0300

    "added Service install"

--- RPI_Service/serv_inst.sh
+++ RPI_Service/serv_inst.sh
@@ -1,23 +1,25 @@
-#! /bin/bash
+#!/bin/bash
 # Script to create 'x' service
 
 SERVICE_NAME="TEST.service"
 DESCRIPTION="TESTING"
 PYTHON_FILE="/home/pi/PythonScripts/Hydro_Pi/TEMP/Pi_switch/Pi_switch_main.py"
 
-cd /lib/systemd/system/
+
 echo "$PWD"
 echo "service name is:"$SERVICE_NAME""
 
-sudo sh -c 'echo " " >> "$SERVICE_NAME"'
-sudo sh -c 'echo "[Unit]" >> "$SERVICE_NAME"'
+echo " " >> "$SERVICE_NAME"
+echo "[Unit]" >> "$SERVICE_NAME"
 
-sudo sh -c 'echo "[Unit]                        "   >> "$SERVICE_NAME"'
-sudo sh -c 'echo "Description="$DESCRIPTION"    "   >> "$SERVICE_NAME"'
-sudo sh -c 'echo "After=multi-user.target       "   >> "$SERVICE_NAME"'
-sudo sh -c 'echo "[Service]                     " >> "$SERVICE_NAME"'
-sudo sh -c 'echo "Type=simple                   " >> "$SERVICE_NAME"'
-sudo sh -c 'echo "ExecStart=/usr/bin/python3 "PYTHON_FILE ${!PYTHON_FILE}"  " >> "$SERVICE_NAME"'
-sudo sh -c 'echo "Restart=on-abort              " >> "$SERVICE_NAME"'
-sudo sh -c 'echo "[Install]                     " >> "$SERVICE_NAME"'
-sudo sh -c 'echo "WantedBy=multi-user.target    " >> "$SERVICE_NAME"'
+echo "[Unit]                        "   >> "$SERVICE_NAME"
+echo "Description="$DESCRIPTION"    "   >> "$SERVICE_NAME"
+echo "After=multi-user.target       "   >> "$SERVICE_NAME"
+echo "[Service]                     " >> "$SERVICE_NAME"
+echo "Type=simple                   " >> "$SERVICE_NAME"
+echo "ExecStart=/usr/bin/python3 "PYTHON_FILE ${!PYTHON_FILE}"  " >> "$SERVICE_NAME"
+echo "Restart=on-abort              " >> "$SERVICE_NAME"
+echo "[Install]                     " >> "$SERVICE_NAME"
+echo "WantedBy=multi-user.target    " >> "$SERVICE_NAME"
+
+cd /lib/systemd/system/
