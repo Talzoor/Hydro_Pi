d7be91c51595d88f37f93f16ddf9312fdc501627
TalzoorBGU <zoor@post.bgu.ac.il>
2018-08-15 00:45:44 +0300

    "added Service install"

--- RPI_Service/serv_inst.sh
+++ RPI_Service/serv_inst.sh
@@ -6,18 +6,4 @@ DESCRIPTION ="TESTING"
 PYTHON_FILE"="/home/pi/PythonScripts/Hydro_Pi/TEMP/Pi_switch/Pi_switch_main.py"
 
 cd /lib/systemd/system/
-sudo sh -c 'echo "
-
-[Unit]
-Description=Hello World
-After=multi-user.target
-
-[Service]
-Type=simple
-ExecStart=/usr/bin/python3 "PYTHON_FILE ${!PYTHON_FILE}"
-Restart=on-abort
-
-[Install]
-WantedBy=multi-user.target
-
-" >> "SERVICE_NAME ${!SERVICE_NAME}"'
+sudo sh -c 'echo "\n\n[Unit]\nDescription=Hello World\nAfter=multi-user.target\n[Service]\nType=simple\nExecStart=/usr/bin/python3 "PYTHON_FILE ${!PYTHON_FILE}"\nRestart=on-abort\n[Install]\nWantedBy=multi-user.target\n\n" >> "SERVICE_NAME ${!SERVICE_NAME}"'
